14da1c173e44bf0f85704f4b74cd763c18d37a20
Michael Meyer <michael.meyer@improving.com>
2021-07-03 20:06:48 -0500

    Initial commit, addition functionality complete. Maybe I should have been a writer.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ISeeDessert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number.");
+            string xString = Console.ReadLine();
+            var x = Decimal.Parse(xString);
+            Console.WriteLine("Enter a second number, and I will add it to the first.");
+            string yString = Console.ReadLine();
+            var y = Decimal.Parse(yString);
+            var answer = Add(x, y);
+            Console.WriteLine($"Result: {answer}");
+        }
+
+        static decimal Add (decimal x, decimal y)
+        {
+            return x + y;
+        }
+    }
+}
