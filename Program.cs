6f839be5813efaaed5901efd423b53de372120c8
Michael Meyer <michael.meyer@improving.com>
2021-07-04 18:15:13 -0500

    completed subtraction functionality after big refactor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ISeeDessert
@@ -12,22 +13,22 @@ namespace ISeeDessert
 
             string equation = Console.ReadLine();
 
-            DoMath(equation);
+            ValidateFormat(equation);
         }
 
-        static void DoMath (string equation)
+        //Checks to see if both the first and last parts of the equation are numbers, then passes all parts on to validate the operand.
+        //This feels kinda bad, but I can't figure out a nice way to refactor the number checks into another method. Don't think I can return null from a Decimal method, which would still result in this method being a mess of logic.
+        static void ValidateFormat (string equation)
         {
             var equationParts = equation.Split(' ').ToList();
             decimal x;
             decimal y;
 
-            if (equationParts.Count() != 3)
-            {
-                Console.WriteLine("Invalid format.");
+            if (!CheckForThree(equationParts)) {
                 return;
             }
-            
-            if (Decimal.TryParse(equationParts.ElementAt(0), out x))
+
+            if (Decimal.TryParse(equationParts.ElementAt(0), out x)) 
             { }
             else
             {
@@ -43,8 +44,55 @@ namespace ISeeDessert
                 return;
             }
 
-            decimal answer = (decimal) x + y;
-            Console.WriteLine($"Result: {answer}");
+            CheckOperand(x, y, equationParts.ElementAt(1));
+        }
+
+        static bool CheckForThree(List<string> equationParts)
+        {
+            if (equationParts.Count() != 3)
+            {
+                Console.WriteLine("Invalid format.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void CheckOperand(decimal x, decimal y, string operand)
+        {
+            List<string> operands = new List<string>()
+            {
+                "+",
+                "-"
+            };
+
+            if (!operands.Contains(operand))
+            {
+                Console.WriteLine("Invalid Operand");
+                return;
+            }
+            else if (operand == "+")
+            {
+                Add(x, y);
+            }
+            else if (operand == "-")
+            {
+                Subtract(x, y);
+            }
+        }
+
+        static void Add(decimal x, decimal y)
+        {
+            decimal result = (decimal)x + y;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        static void Subtract(decimal x, decimal y)
+        {
+            decimal result = (decimal)x - y;
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
