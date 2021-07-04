5e9dfb4fa1493b3b2f055388f53ba9e9f73f8cd5
Michael Meyer <michael.meyer@improving.com>
2021-07-03 21:04:38 -0500

    After much headache due to a small typo, I can conclude I probably should not have been a writer (and definitely not an editor)

--- Program.cs
+++ Program.cs
@@ -6,19 +6,39 @@ namespace ISeeDessert
     {
         static void Main(string[] args)
         {
+            decimal x = 0;
+            decimal y = 0;
+
+            //First variable
             Console.WriteLine("Enter a Number.");
-            string xString = Console.ReadLine();
-            var x = Decimal.Parse(xString);
+            var firstInput = Console.ReadLine();
+            if (Decimal.TryParse(firstInput, out x))
+            { } 
+            else 
+            { 
+                Console.WriteLine($"The first value, '{firstInput}', was not a number.");
+                return;
+            }
+
+            //Second variable
             Console.WriteLine("Enter a second number, and I will add it to the first.");
-            string yString = Console.ReadLine();
-            var y = Decimal.Parse(yString);
-            var answer = Add(x, y);
-            Console.WriteLine($"Result: {answer}");
+            string secondInput = Console.ReadLine();
+            if (Decimal.TryParse(secondInput, out y))
+            { }
+            else
+            {
+                Console.WriteLine($"The second value, '{secondInput}', was not a number.");
+                return;
+            }
+
+            //Write answer
+            Add(x, y);
         }
 
-        static decimal Add (decimal x, decimal y)
+        static void Add (decimal x, decimal y)
         {
-            return x + y;
+            var result = (decimal)x + y;
+            Console.WriteLine($"result: {result}");
         }
     }
 }
